637f0cfcc9e8f001bf2bbb727df14c05e8cbe05b
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-06-14 14:34:41 -0700

    commit

--- Classes/Classes/classApp/models.py
+++ Classes/Classes/classApp/models.py
@@ -15,3 +15,10 @@ class DjangoClasses(models.Model):
 
 
 
+class1 = DjangoClasses(title="Math",name="Math201", courseNumber=201,duration=2.5)
+class1.save()
+class2 = DjangoClasses(title="History",name="History101", courseNumber=101,duration=1.5)
+class2.save()
+class3 = DjangoClasses(title="Anatomy",name="Bio401", courseNumber=401,duration=3.5)
+class3.save()
+
